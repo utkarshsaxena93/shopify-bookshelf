7b32c27a5828e711e1747d96e02f4331ee66c2ec
utkarshsaxena <utkarshsaxena93@gmail.com>
2016-08-05 21:08:56 -0700

    Add sendgrid functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  # Settings for SENDGRID ADDON HEROKU
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'shopify-bookshelf.herokuapp.com/', :protocol => 'https'}
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
