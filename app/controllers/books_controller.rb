939ffa53e28a7f427f436cde42806768793b13bf
utkarshsaxena <utkarshsaxena93@gmail.com>
2016-08-05 23:14:42 -0700

    user and book associations working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,10 +26,11 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = Book.new(book_params)
+    @book.users << current_user
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.html { redirect_to user_dashboard_path, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
