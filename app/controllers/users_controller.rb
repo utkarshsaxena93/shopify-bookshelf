5835bbf2e4cb08d1286e4d5bb2e7f618e23ac26e
utkarshsaxena <utkarshsaxena93@gmail.com>
2016-09-03 22:37:02 -0400

    Move add books to dropdown + user can only invite shopify users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,15 +28,21 @@ class UsersController < ApplicationController
   end
 
   def create_invitation
-    userExists = User.where(email: params[:email])
-    invitationAccepted = user.pluck(:invitation_accepted_at)
+    userEmail = params[:email]
+    userExists = User.where(email: userEmail)
+    invitationAccepted = userExists.pluck(:invitation_accepted_at)
+    byebug
 
     if userExists.empty?
-      User.invite!({:email => params[:email]}, current_user)
-      redirect_to user_dashboard_path, notice: "Invitation successfully sent."
+      if userEmail.include? "@shopify.com"
+        User.invite!({:email => userEmail}, current_user)
+        redirect_to user_dashboard_path, notice: "Invitation successfully sent."
+      else
+        redirect_to user_dashboard_path, alert: "Invalid email. You can only invite users with @shopify email account.."
+      end
     elsif !userExists.empty? and ! invitationAccepted.any?
-      User.invite!({:email => params[:email]}, current_user)
-      redirect_to user_dashboard_path, alert: "User has already been invited. Seding a fresh invite."
+      User.invite!({:email => userEmail}, current_user)
+      redirect_to user_dashboard_path, alert: "User has already been invited. Sending a fresh invite."
     else
       redirect_to user_dashboard_path, alert: "User already exists. No invitation sent."
     end
