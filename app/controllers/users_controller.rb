c276795b61889ca805f998ffbac01c7681dd9819
utkarshsaxena <utkarshsaxena93@gmail.com>
2016-09-08 22:05:04 -0400

    Style browse user page. Change share a quote to recommend a book

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @users = User.all.where.not(id: current_user.id)
+    @users = User.where.not(id: current_user.id).paginate(:page => params[:page], per_page: 50).order('id DESC')
   end
 
   def show
