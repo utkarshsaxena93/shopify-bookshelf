98b0d92216bfc4d8993e9505da3bba2953954a70
utkarshsaxena <utkarshsaxena93@gmail.com>
2016-09-22 01:45:11 -0400

    Temporarily remove tracking from books_readings.rb models

--- app/models/books_reading.rb
+++ app/models/books_reading.rb
@@ -3,9 +3,7 @@ class BooksReading < ActiveRecord::Base
   before_destroy :pause_tracking
   after_destroy :resume_activities
 
-  include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.current_user },
-          title: Proc.new { |controller, model| model.title }
+  
 
   def pause_tracking
     BooksReading.public_activity_off
