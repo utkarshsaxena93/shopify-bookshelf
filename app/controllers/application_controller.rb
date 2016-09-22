99374ebeb2023179466623c0669f3c96985696f9
utkarshsaxena <utkarshsaxena93@gmail.com>
2016-09-22 01:18:06 -0400

    Fix invitation page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,20 @@ class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!, :gravitarhash
+  before_action :authenticate_user!, :gravitarhash, :devise_configure_permitted_parameters, if: :devise_controller?
 
   def gravitarhash
     if current_user
       @gravitarhash = current_user.gravitarhash
+    end
+  end
+
+  protected
+
+  def devise_configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:accept_invitation) do |u|
+      u.permit(:name, :position, :password, :password_confirmation,
+               :invitation_token)
     end
   end
 
