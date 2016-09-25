744ae650ba47849e7ad80e1402d55f5d0e8c04a2
utkarsh saxena <utkarsh.saxena@shopify.com>
2016-09-25 18:59:06 -0400

    Allow a user to edit its profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,10 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :position)
+  end
+  
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 end
