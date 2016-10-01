e1c5cdfb99b43b658a6f7a6e3076504d50783590
utkarsh saxena <utkarsh.saxena@shopify.com>
2016-10-01 17:52:56 -0400

    redirect to books_path on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :devise_configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    books_path
+  end
+
   protected
 
   def devise_configure_permitted_parameters
